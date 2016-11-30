fa920cf665db01aabb9373175b65a9c8357238bf
pandeyshubham75 <pandeyshubham75@gmail.com>
2016-12-01 02:26:14 +0530

    Header Changes

--- resources/views/showvideo.blade.php
+++ resources/views/showvideo.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Videos Uploaded by You</div>
+                <div class="panel-heading">VideoCenter</div>
 
                 @include('layouts.navbar')
 
